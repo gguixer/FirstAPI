4af4256af714100444e7058104187f540f79e885
DESKTOP-2F50K0C\gguix <gguixer@gmail.com>
2022-10-06 15:52:19 +0200

    Agregar archivos de proyecto.

---
+++ Context/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using FirstAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+
+        }
+
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FirstAPI.Context;
+using FirstAPI.Models;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ProductsController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+        {
+            return await _context.Product.ToListAsync();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var Product = await _context.Product.FindAsync(id);
+
+            if (Product == null)
+            {
+                return NotFound();
+            }
+
+            return Product;
+        }
+
+        // PUT: api/Products/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product Product)
+        {
+            if (id != Product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(Product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product Product)
+        {
+            _context.Product.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = Product.Id }, Product);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var Product = await _context.Product.FindAsync(id);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Product.Remove(Product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Product.Any(e => e.Id == id);
+        }
+    }
+}
